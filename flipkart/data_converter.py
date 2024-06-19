c6597f100839e1a040b19f5e98eddd6666e1da60
Girdhar Saini <girdharscs@gmail.com>
2024-06-19 18:45:00 +0200

    Add type hints to data converter

--- flipkart/data_converter.py
+++ flipkart/data_converter.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from langchain_core.documents import Document
+from typing import List
+
 
 class DataConverter:
-    def __init__(self , file_path:str):
+
+    def __init__(self, file_path: str) -> None:
         self.file_path = file_path
-        
-    def convert(self):
-        df = pd.read_csv(self.file_path)[["product_title","review"]]   
+
+    def convert(self) -> List[Document]:
+        df = pd.read_csv(self.file_path)[["product_title", "review"]]
 
         docs = [
-            Document(page_content=row['review'] ,
-                    metadata = {"product_name" : row["product_title"]})
+            Document(
+                page_content=row["review"],
+                metadata={"product_name": row["product_title"]},
+            )
             for _, row in df.iterrows()
         ]
 
