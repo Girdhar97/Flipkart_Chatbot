153f3048714942a2664de7ed45a6489eb335ff00
Girdhar Saini <girdharscs@gmail.com>
2024-09-15 18:50:00 +0200

    Make port configurable via env var

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def get_bot_response() -> str:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.getenv("APP_PORT", "8000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
