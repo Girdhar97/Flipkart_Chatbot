f7a98abbb47aff44cfc018c003fe01d36072a243
Girdhar Saini <girdharscs@gmail.com>
2025-01-31 20:15:00 +0100

    Setup file with Description + Keywords + Classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="FLIPKART RECOMMENDER CHATBOT",
-    version="0.1",
+    name="flipkart-recommender-chatbot",  # Lowercase, normalized
+    version="0.1.0",
     author="Girdhar97",
+    description="Flipkart-style RAG chatbot with AstraDB and LangGraph",  # NEW: Brief desc
+    keywords="RAG chatbot langchain astradb flask",  # NEW: Searchable tags
     packages=find_packages(),
-    install_requires = requirements,
+    install_requires=requirements,
+    classifiers=[  # NEW: Standards compliance
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
